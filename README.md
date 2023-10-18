d0544f96fb4db35184718a40a44f1816ffd8ded9
Christoph Sommer <christoph.sommer@ist.ac.at>
2023-10-18 13:47:41 +0200

    cleanup and doc

--- README.md
+++ README.md
@@ -1,7 +1,34 @@
 # rotalign - rotate volume movies to match landmarks
 ---
 
+## Background
+*rotalign* rotates 3D-movies (.tif) to standardize the position of three points. The three points **CM**, **PB**, and **SA** are supplied in an separate coordinates (.csv). Units are in microns.
+
+First, the volume of each time frame is resampled to isotropic resolution. The pixel sizes are taken from the ImageJ metadata saved inside the .tif file. Then, the volume is shifted, such that **CM** is in the center of the volume. The volume is rotated such that the plane containing **CM**, **PB**, and **SA** is the central Z-plane and **PB** is pointing up in the Y-axis. 
+
+The coordinates file name needs to contain the header column names:
+
+```
+"Frame #",     # frame index (starting from 1)
+"name",        # name of the .tif image
+"Xcm",         # X position of CM in micron 
+"Ycm",         # ...
+"Zcm",         # ...
+"Xpb",         # X position of PB in micron
+"Ypb",         # ...
+"Zpb",         # ...
+"Xsa",         # X position of SA in micron
+"Ysa",         # ...
+"Zsa"          # ...
+```
+
+"Frame #" may contain two positions for **SA** for a single time frame. In that case **SA** is set as the mean location of the two.
+
+The ouput movie is writting as .tif file using the suffix **"_aligned.tif"**. Only time frames contained in the coordinates file are processed and exported.
+
 ## Usage
+
+```rotalign --coords <coordinate-file.csv> <movie_1.tif> <movie_2.tif>...```
 
 
 ## Setup
@@ -9,6 +36,8 @@
 ```
 # create separate python environment 
 conda create -n rotalign_env python=3.9 pip
+
+# activate environment
 conda activate rotalign_env
 
 # install lastest version from github
--- rotalign/cli.py
+++ rotalign/cli.py
@@ -12,39 +12,41 @@ from scipy import ndimage as ndi
 
 from matplotlib import pyplot as plt
 
+
 def read_tiff_voxel_size_zyx(file_path):
     """
     Implemented based on information found in https://pypi.org/project/tifffile
     """
 
     def _xy_voxel_size(tags, key):
-        assert key in ['XResolution', 'YResolution']
+        assert key in ["XResolution", "YResolution"]
         if key in tags:
             num_pixels, units = tags[key].value
             return units / num_pixels
         # return default
-        return 1.
+        return 1.0
 
     with tifffile.TiffFile(file_path) as tiff:
         image_metadata = tiff.imagej_metadata
         if image_metadata is not None:
-            z = image_metadata.get('spacing', 1.)
+            z = image_metadata.get("spacing", 1.0)
         else:
             # default voxel size
-            z = 1.
+            z = 1.0
 
         tags = tiff.pages[0].tags
         # parse X, Y resolution
-        y = _xy_voxel_size(tags, 'YResolution')
-        x = _xy_voxel_size(tags, 'XResolution')
+        y = _xy_voxel_size(tags, "YResolution")
+        x = _xy_voxel_size(tags, "XResolution")
         # return voxel size
         return np.array([z, y, x])
+
 
 def umeyama(P, Q):
     assert P.shape == Q.shape
     n, dim = P.shape
 
-    centeredP = P 
+    centeredP = P
     centeredQ = Q
 
     C = np.dot(np.transpose(centeredP), centeredQ) / n
@@ -61,13 +63,13 @@ def umeyama(P, Q):
 
     return R
 
+
 def normvec(vec):
     return vec / np.linalg.norm(vec)
 
+
 def get_args():
-    parser = argparse.ArgumentParser(
-        description="Rotational alignment"
-    )
+    parser = argparse.ArgumentParser(description="Rotational alignment")
     parser.add_argument(
         "input_tifs",
         type=str,
@@ -87,6 +89,21 @@ def get_args():
     return parser.parse_args()
 
 
+REQ_COLUMNS = [
+    "Frame #",
+    "name",
+    "Xcm",
+    "Ycm",
+    "Zcm",
+    "Xpb",
+    "Ypb",
+    "Zpb",
+    "Xsa",
+    "Ysa",
+    "Zsa",
+]
+
+
 def run(mov_in_fn, coord_fn):
     assert os.path.exists(mov_in_fn), f"Tif file '{mov_in_fn}' not found"
     assert os.path.exists(coord_fn), f"Coord file '{coord_fn}' not found"
@@ -95,37 +112,41 @@ def run(mov_in_fn, coord_fn):
     tif_base_fn, _ = os.path.splitext(tif_fn)
 
     print(f"Reading file {tif_fn}")
-    print("*"*80)
+    print("*" * 80)
     mov_in_raw = tifffile.imread(mov_in_fn)
     z_pxs, y_pxs, x_pxs = read_tiff_voxel_size_zyx(mov_in_fn)
 
     COORDS_TAB = pd.read_csv(coord_fn)
 
-    COORDS_TAB = COORDS_TAB[['Frame #', 'Time (seconds post fertilisation', 'name', 'Xcm', 'Ycm','Zcm', 'Xpb', 'Ypb', 'Zpb', 'Xsa', 'Ysa', 'Zsa']]
-    COORDS_TAB["time"] = COORDS_TAB["Time (seconds post fertilisation"]
+    for c in REQ_COLUMNS:
+        assert (
+            c in COORDS_TAB.columns
+        ), f"Column '{c}' not in found in file '{coord_fn}'"
+
+    COORDS_TAB = COORDS_TAB[REQ_COLUMNS]
     COORDS_TAB["frame"] = COORDS_TAB["Frame #"] - 1
 
     coords_tab = COORDS_TAB[COORDS_TAB["name"] == tif_base_fn].sort_values("frame")
     if len(coords_tab) == 0:
         print(f"Movie {tif_base_fn} not found in coords file {coord_fn}. Skipping...")
-        return 
-    
+        return
+
     t_min = coords_tab.frame.min()
     t_max = coords_tab.frame.max()
     coords_tab["frame"] = coords_tab["frame"] - t_min
-    
-    coords_tab = coords_tab.groupby("frame")[['time', 'Xcm', 'Ycm','Zcm', 'Xpb', 'Ypb', 'Zpb', 'Xsa', 'Ysa', 'Zsa']].mean()
+
+    coords_tab = coords_tab.groupby("frame")[
+        ["Xcm", "Ycm", "Zcm", "Xpb", "Ypb", "Zpb", "Xsa", "Ysa", "Zsa"]
+    ].mean()
 
     # crop time
     t_size, z_size, c_size, y_size, x_size = mov_in_raw.shape
 
-    
-
-    mov_in_raw = mov_in_raw[t_min:t_max+1, ...]
+    mov_in_raw = mov_in_raw[t_min : t_max + 1, ...]
 
     t_size = mov_in_raw.shape[0]
 
-    # 
+    #
 
     coords_tab["Xsa_px"] = (coords_tab["Xsa"] / x_pxs).astype(int)
     coords_tab["Ysa_px"] = (coords_tab["Ysa"] / y_pxs).astype(int)
@@ -143,51 +164,72 @@ def run(mov_in_fn, coord_fn):
 
     for t in trange(t_size, desc="  - centering to CM"):
         for c in range(c_size):
-            mov_slice = mov_in_raw[t,:, c, :, :]
-            z_sft = z_size//2 - coords_tab.loc[t].Zcm_px 
-            y_sft = y_size//2 - coords_tab.loc[t].Ycm_px 
-            x_sft = x_size//2 - coords_tab.loc[t].Xcm_px 
-            shift(mov_slice, shift=[z_sft, y_sft, x_sft], order=0, output=mov_centered[t,:,c,...])
+            mov_slice = mov_in_raw[t, :, c, :, :]
+            z_sft = z_size // 2 - coords_tab.loc[t].Zcm_px
+            y_sft = y_size // 2 - coords_tab.loc[t].Ycm_px
+            x_sft = x_size // 2 - coords_tab.loc[t].Xcm_px
+            shift(
+                mov_slice,
+                shift=[z_sft, y_sft, x_sft],
+                order=0,
+                output=mov_centered[t, :, c, ...],
+            )
 
-    z_iso_size = int(round((z_pxs / x_pxs)*z_size))
+    z_iso_size = int(round((z_pxs / x_pxs) * z_size))
     mov_iso = np.zeros((t_size, z_iso_size, c_size, y_size, x_size), dtype=np.uint8)
     for t in trange(t_size, desc="  - resampling to isotropic resolution"):
         for c in range(c_size):
-            mov_iso[t, :, c, ...] = ndi.zoom(mov_centered[t, :, c, ...], (z_pxs / x_pxs, 1, 1), order=1)
+            mov_iso[t, :, c, ...] = ndi.zoom(
+                mov_centered[t, :, c, ...], (z_pxs / x_pxs, 1, 1), order=1
+            )
 
     mov_out = np.zeros((t_size, z_iso_size, c_size, y_size, x_size), np.uint8)
 
     for t in trange(t_size, desc="  - Aligning frames...", position=1):
-        vec_sa = ([coords_tab["Zsa"].loc[t] - coords_tab["Zcm"].loc[t],
-                coords_tab["Ysa"].loc[t] - coords_tab["Ycm"].loc[t],
-                coords_tab["Xsa"].loc[t] - coords_tab["Xcm"].loc[t]])
+        vec_sa = [
+            coords_tab["Zsa"].loc[t] - coords_tab["Zcm"].loc[t],
+            coords_tab["Ysa"].loc[t] - coords_tab["Ycm"].loc[t],
+            coords_tab["Xsa"].loc[t] - coords_tab["Xcm"].loc[t],
+        ]
 
-        vec_pb = ([coords_tab["Zpb"].loc[t] - coords_tab["Zcm"].loc[t],
-                coords_tab["Ypb"].loc[t] - coords_tab["Ycm"].loc[t],
-                coords_tab["Xpb"].loc[t] - coords_tab["Xcm"].loc[t]])
-        
+        vec_pb = [
+            coords_tab["Zpb"].loc[t] - coords_tab["Zcm"].loc[t],
+            coords_tab["Ypb"].loc[t] - coords_tab["Ycm"].loc[t],
+            coords_tab["Xpb"].loc[t] - coords_tab["Xcm"].loc[t],
+        ]
+
         vec_sa_norm = normvec(vec_sa)
         vec_pb_norm = normvec(vec_pb)
 
-        
         P = np.array([vec_pb_norm, np.cross(vec_sa_norm, vec_pb_norm)])
-        Q = np.array([[0,-1,0], [-1,0,0]])
-        
-        R = umeyama(P,Q)
-        
-        mg = np.mgrid[-z_iso_size//2:z_iso_size//2, -y_size//2:y_size//2, -x_size//2:x_size//2]
-        mg_rot = R.dot(mg.reshape(3, -1)).reshape(mg.shape) 
-        
-        cc = (mg_rot.T + np.array([z_iso_size//2, 256,256])).T
+        Q = np.array([[0, -1, 0], [-1, 0, 0]])
+
+        R = umeyama(P, Q)
+
+        mg = np.mgrid[
+            -z_iso_size // 2 : z_iso_size // 2,
+            -y_size // 2 : y_size // 2,
+            -x_size // 2 : x_size // 2,
+        ]
+        mg_rot = R.dot(mg.reshape(3, -1)).reshape(mg.shape)
+
+        cc = (mg_rot.T + np.array([z_iso_size // 2, 256, 256])).T
 
         for c in trange(c_size, desc="  |_ channel...", position=1, leave=False):
-            mov_t = tf.warp(mov_iso[t,:, c], cc, preserve_range=True, order=1)
-            mov_out[t,:, c, :, :] = mov_t
+            mov_t = tf.warp(mov_iso[t, :, c], cc, preserve_range=True, order=1)
+            mov_out[t, :, c, :, :] = mov_t
 
     print(f"Saving output...")
-    print("*"*80)
-    tifffile.imsave(f"{tif_path}/{tif_base_fn}_aligned.tif", mov_out, imagej=True, resolution=(1./x_pxs, 1./y_pxs), metadata={'spacing': x_pxs, 'unit': 'um', 'axes': 'TZCYX'})
+    print("*" * 80)
+    tifffile.imsave(
+        f"{tif_path}/{tif_base_fn}_aligned.tif",
+        mov_out,
+        imagej=True,
+        resolution=(1.0 / x_pxs, 1.0 / y_pxs),
+        metadata={"spacing": x_pxs, "unit": "um", "axes": "TZCYX"},
+    )
     print()
+
 
 def main():
     args = get_args()
@@ -201,8 +243,6 @@ def main():
     for fn in args.input_tifs:
         run(os.path.abspath(fn), args.coords[0])
 
+
 if __name__ == "__main__":
     main()
-
-
-
